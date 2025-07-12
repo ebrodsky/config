b4ea78a4f03361eefd6c6a840e8a6893202d749d
Eytan Brodsky <eytanbrodsky@gmail.com>
2025-07-12 14:42:43 -0700

    change color when vpn connected

--- waybar/scripts/vpninfo.sh
+++ waybar/scripts/vpninfo.sh
@@ -5,7 +5,7 @@
 if ip addr show client 2>/dev/null | grep -q "inet "; then
     # Get basic info without sudo
     INTERFACE_INFO=$(ip addr show client 2>/dev/null | grep "inet " | head -1 | awk '{print $2}')
-    echo "{\"text\": \"VPN: Connected\", \"tooltip\": \"Interface: client\\nIP: $INTERFACE_INFO\"}"
+    echo "{\"text\": \"VPN: Connected\", \"tooltip\": \"Interface: client\\nIP: $INTERFACE_INFO\", \"class\": \"connected\"}"
 else
     echo '{"text": "VPN: Not connected", "tooltip": "Not connected to a VPN"}'
 fi
